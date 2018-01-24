a7325a21410ecfdbe55e24895d2f6950a1b5f381
alfateam123 <alfateam123@nwa.xyz>
2018-01-24 23:08:49 +0100

    feat(booklist): improve visualization of single book boxes

--- src/BookList.js
+++ src/BookList.js
@@ -1,10 +1,16 @@
 import * as React from "react";
 
 import {SingleBook} from "./SingleBook";
+import "./BookList.css";
 
 export class BookList extends React.Component {
   render(){
-    return this.props.books.map(b => <SingleBook key={b.title.replace(/ /g, "_")}
-                                               title={b.title} />);
+    const singleBooks = this.props.books.map(
+    (b, i) => <SingleBook key={b.title.replace(/ /g, "_")+i}
+        title={b.title}
+        series={b.series}
+        number={b.volume_number}
+    />);
+    return <div className="book-list">{singleBooks}</div>;
   }
 }
--- src/SingleBook.js
+++ src/SingleBook.js
@@ -1,12 +1,18 @@
 import * as React from "react";
 
 export class SingleBook extends React.Component {
+  requestFilteringOnSeries = (e) => {
+    console.log("filter on series", e, this.props.series);
+  }
+
   render() {
     console.log("singlebook", this.props);
 
-    return <div>
-      <p style={{fontWeight: "bolder"}}>{this.props.title}</p>
-      <p>{this.props.author || "Author: n/a"}</p>
+    return <div className="book-item">
+      <p className="book-description">
+        <span onClick={this.requestFilteringOnSeries}>
+            {this.props.series}</span> - {this.props.number === 0 ? "(unique)" : this.props.number}
+      </p>
     </div>;
   }
 }
