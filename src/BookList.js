1527f0d294e1b019ffd0df011fe433c2b15ca39c
alfateam123 <alfateam123@nwa.xyz>
2018-01-27 23:28:00 +0100

    fix(booklist): remove title property from books

--- src/BookList.js
+++ src/BookList.js
@@ -6,8 +6,7 @@ import "./BookList.css";
 export class BookList extends React.Component {
   render(){
     const singleBooks = this.props.books.map(
-    (b, i) => <SingleBook key={b.title.replace(/ /g, "_")+i}
-        title={b.title}
+    (b, i) => <SingleBook key={b.series.replace(/ /g, "_")+b.volume_number+i}
         series={b.series}
         number={b.volume_number}
         image={b.image}
